198865e8a571ab2013fdc0b2723db7bb235b7170
Marek Ozimina <marek@redguardian.pl>
2022-03-20 18:00:14 +0100

    Change notificaition

--- src/cron.js
+++ src/cron.js
@@ -2,12 +2,7 @@ const cron = require('node-cron');
 const oneSignal = require('onesignal-node');  
 const oneSignalClient = new oneSignal.Client("af840bb4-2f85-42a0-8632-a7f65367ddb5", process.env.ONE_SIGNAL_API_KEY);
 const notification = {
-    contents: {
-      'en': 'Sprawdź czy dasz radę je odgadnąć!'
-    },
-    headings: {
-        "en": 'Nowe słówko dostępne'
-    },
+    template_id: "e48e3d12-7262-4464-8068-a7ab46f5bfdf",
     included_segments: ['Subscribed Users']
   };
 
