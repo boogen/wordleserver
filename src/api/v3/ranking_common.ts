bcde339aba3356508495db2fae7ce02a31bba57c
Marek Ozimina <github@redguardian.pl>
2022-09-19 22:39:11 +0200

    Debug

--- src/api/v3/ranking_common.ts
+++ src/api/v3/ranking_common.ts
@@ -4,6 +4,7 @@ import { get_nick } from "./player_common";
 
 export async function get_ranking(player_id:number, ranking:RankingEntry[], dbi:WordleDBI):Promise<RankingReply> {
     var ranking_with_nicks:PositionInRank[] = await Promise.all(ranking.map(async (e) => new PositionInRank(e.position, e.score, (await get_nick(e.player_id, dbi)).nick, player_id)))
+    console.log(ranking_with_nicks)
     return new RankingReply(ranking_with_nicks.find(e => e.player_id === player_id), ranking_with_nicks);
 }
 
