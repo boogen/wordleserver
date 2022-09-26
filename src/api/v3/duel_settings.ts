18d96e4cf7b0ef599a1605d89d9ae33558344961
Marek Ozimina <github@redguardian.pl>
2022-09-26 22:14:33 +0200

    Tweak duels

--- src/api/v3/duel_settings.ts
+++ src/api/v3/duel_settings.ts
@@ -4,5 +4,5 @@ export const ELO_COEFFICIENT:number = 50;
 export const DUEL_DURATION:number = 180;
 export const DEFAULT_ELO:number = 1200;
 export const MATCH_ELO_DIFF:number = 400;
-export const NUMBER_OF_LAST_OPPONENTS_TO_EXCLUDE:number = 10;
+export const NUMBER_OF_LAST_OPPONENTS_TO_EXCLUDE:number = 0;
 export const BOT_THRESHOLD:MinMax = new MinMax(0.0001, 0.001);
