0ece0342fc16e6e655ed0de9913a0ec92c37eefe
Marcin Bugala <marcin.bugala@tensquaregames.com>
2022-10-03 18:56:09 +0200

    metrics

---
+++ src/metrics.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+import { collectDefaultMetrics, register } from 'prom-client';
+
+collectDefaultMetrics();
+
+export const router = express.Router();
+
+router.get('/metrics', async (req, res, next) => {
+  try {
+    res.set('Content-Type', register.contentType);
+    res.end(await register.metrics());
+  } catch (err) {
+    res.status(500).end(err);
+  }
+});
