dddd03fd5dadaa066c4214f17c14930f07fa09d1
Marek Ozimina <github@redguardian.pl>
2022-11-15 10:45:32 +0100

    Fix case

--- src/api/v3/player.ts
+++ src/api/v3/player.ts
@@ -67,10 +67,10 @@ player.post("/getProfile", async (req:express.Request, res:express.Response, nex
     try {
         const value = new ProfileRequest(req);
         const player_id = await dbi.resolvePlayerId(value.authId);
-        console.log("Getting profile for player: " + value.playerId)
-        const profile = await dbi.getProfile(value.playerId);
-        const duel_stats = await dbi.getSpellingBeeDuelStats(player_id, value.playerId)
-        const spelling_bee_stats = await dbi.getSpellingBeeStats(value.playerId)
+        console.log("Getting profile for player: " + value.player_id)
+        const profile = await dbi.getProfile(value.player_id);
+        const duel_stats = await dbi.getSpellingBeeDuelStats(player_id, value.player_id)
+        const spelling_bee_stats = await dbi.getSpellingBeeStats(value.player_id)
         if (profile === null) {
             res.json({message: null});
             return;
--- src/types/ProfileRequest.ts
+++ src/types/ProfileRequest.ts
@@ -1,19 +1,19 @@
 import joi from '@hapi/joi';
 import express from 'express';
+import AuthIdRequest from './AuthIdRequest';
 
 const profileSchema = joi.object({
     authId: joi.string().trim().required(),
     playerId: joi.number().integer().required()
 });
 
-export default class ProfileRequest {
-    authId:string;
-    playerId:number;
+export default class ProfileRequest extends AuthIdRequest {
+    player_id:number;
 
     constructor(req:express.Request) {
+        super(req);
         profileSchema.validate(req.body);
-        this.authId = req.body.authId;
-        this.playerId = Number.parseInt(req.body.playerId);
+        this.player_id = Number.parseInt(req.body.playerId);
     }
 }
 
